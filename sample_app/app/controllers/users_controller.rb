2439bc52b1e0711497590aaaad567ebcaf96cf58
priyankawgbl <priyanka.singhal@wgblindia.com>
2018-07-25 19:43:00 +0530

    till chapter 10 rest few

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,28 +1,63 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.all
-  end
-  def new
-    @user = User.new
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+   def index
+    @users = User.paginate(page: params[:page])
   end
   def show
     @user = User.find(params[:id])
   end
-   def create
-    @user = User.new(user_params)   # Not the final implementation!
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-   private
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    # Before filters
 
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+         store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+     # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 end
